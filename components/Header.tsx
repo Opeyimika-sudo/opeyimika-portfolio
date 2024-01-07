2b57ef751d0682b6d538e9c6e343b8d10262d32e
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2024-01-07 03:51:46 +0100

    init: first commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+      <div>
+        <h2>̣Opéyímiká</h2>
+        <div>
+            <a href=''>Home</a>  
+            <a href='#about`'>About</a>
+            <a href='#projects'>Projects</a>
+            <a href='#contact'>Contact</a>
+            <a href='#resume'>Footer</a>
+        </div>
+        <button>View Resume</button>
+    </div>
+    )
+}
+
+export default Header
